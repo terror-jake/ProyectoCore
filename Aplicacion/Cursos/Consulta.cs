e855e33ffc5dc28f3f56b9711aa69277140978c6
Jose Luis Alvarez <joseluis_fps@hotmail.com>
2021-05-10 19:28:17 -0500

    DTO 1

--- Aplicacion/Cursos/Consulta.cs
+++ Aplicacion/Cursos/Consulta.cs
@@ -10,8 +10,8 @@ namespace Aplicacion.Cursos
 {
     public class Consulta
     {
-        public class ListaCursos : IRequest<List<Curso>> {}
-        public class Manejador : IRequestHandler<ListaCursos, List<Curso>>
+        public class ListaCursos : IRequest<List<CursoDTO>> {}
+        public class Manejador : IRequestHandler<ListaCursos, List<CursoDTO>>
         {
             private readonly CursosOnlineContext context;
             public Manejador(CursosOnlineContext _context)
@@ -19,9 +19,13 @@ namespace Aplicacion.Cursos
                 this.context = _context;   
             }
 
-            public async Task<List<Curso>> Handle(ListaCursos request, CancellationToken cancellationToken)
+            public async Task<List<CursoDTO>> Handle(ListaCursos request, CancellationToken cancellationToken)
             {
-                var cursos = await context.Curso.ToListAsync();
+                var cursos = await context.Curso
+                    .Include(x => x.InstructoresLink)
+                    .ThenInclude(x => x.Instructor)
+                    .ToListAsync();
+
                 return cursos;
             }
         }
