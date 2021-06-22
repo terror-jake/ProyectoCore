c47c53ee7a942dc7bcd4a0cec224233441576506
terror-jake <joseluis_fps@hotmail.com>
2021-06-22 17:55:45 -0500

    Comentarios 1

---
+++ Aplicacion/Comentarios/Nuevo.cs
@@ -0,0 +1,14 @@
+using MediatR;
+
+namespace Aplicacion.Comentarios
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Alumno { get; set; }
+            public int Puntaje { get; set; }
+            public string Comentario { get; set; }
+        }
+    }
+}
